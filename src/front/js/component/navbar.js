d060b2f0e3b8141ac7d8c275aeda674a840a6355
josecalcru <josecalcru@gmail.com>
2021-04-19 05:28:46 +0000

    animación ver 1

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,23 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+	const [menuDisplay, setMenuDisplay] = useState(false);
+	let fadeClass = undefined;
+	let openClass = undefined;
+
+	if (menuDisplay === true) {
+		fadeClass = "fade";
+		openClass = "open";
+	} else {
+		fadeClass = "";
+		openClass = "";
+	}
+
 	return (
 		<nav>
-			<div className="hamburger">
+			<div className="hamburger" onClick={() => setMenuDisplay(!menuDisplay)}>
 				<div className="line" />
 				<div className="line" />
 				<div className="line" />
 			</div>
-			<ul className="nav-links">
-				<li>
+			<ul className={"nav-links " + openClass}>
+				<li className={fadeClass}>
 					<a href="#">About</a>
 				</li>
-				<li>
+				<li className={fadeClass}>
 					<a href="#">Contact</a>
 				</li>
-				<li>
-					<a href="#">Projects</a>
+				<li className={fadeClass}>
+					<a href="#">Échele miel</a>
 				</li>
 			</ul>
 		</nav>

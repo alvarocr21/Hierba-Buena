4a5b71b3b2b991ee1b2b9b81f5c39c29b560eb20
alvarocr21 <alvarocr21@gmail.com>
2021-05-04 02:01:06 +0000

    Carousel primera parte to be continue

--- src/front/js/pages/blog/blog.js
+++ src/front/js/pages/blog/blog.js
@@ -2,49 +2,49 @@ import React, { useContext } from "react";
 import { Context } from "../../store/appContext";
 import "../../../styles/_home.scss";
 import { Link } from "react-router-dom";
+import { Carousel } from "react-bootstrap";
 export const Blog = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
 		<div>
-			<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
-				<ol className="carousel-indicators">
-					<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active" />
-					<li data-target="#carouselExampleIndicators" data-slide-to="1" />
-					<li data-target="#carouselExampleIndicators" data-slide-to="2" />
-				</ol>
-				<div className="carousel-inner">
-					<div className="carousel-item active mx-3 my-3">
-						<img
-							src="https://www.biotropic.com/images/news/2019/beitragsbilder/PPP-CostaRica_1200px.jpg"
-							className="d-block w-100"
-							alt="..."
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							src="https://c8.alamy.com/compes/x54ny5/agricultor-cuttting-pina-costa-rica-x54ny5.jpg"
-							className="d-block w-100"
-							alt="..."
-						/>
-					</div>
-					<div className="carousel-item">
-						<img
-							src="https://revistasumma.com/wp-content/uploads/2019/07/agro-1080x675.jpg"
-							className="d-block w-100"
-							alt="..."
-						/>
-					</div>
-				</div>
-				<a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-					<span className="carousel-control-prev-icon" aria-hidden="true" />
-					<span className="sr-only">Previous</span>
-				</a>
-				<a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-					<span className="carousel-control-next-icon" aria-hidden="true" />
-					<span className="sr-only">Next</span>
-				</a>
-			</div>
+			<Carousel>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://www.biotropic.com/images/news/2019/beitragsbilder/PPP-CostaRica_1200px.jpg"
+						alt="First slide"
+					/>
+					<Carousel.Caption>
+						<h3>First slide label</h3>
+						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://c8.alamy.com/compes/x54ny5/agricultor-cuttting-pina-costa-rica-x54ny5.jpg"
+						alt="Second slide"
+					/>
+
+					<Carousel.Caption>
+						<h3>Second slide label</h3>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img
+						className="d-block w-100"
+						src="https://revistasumma.com/wp-content/uploads/2019/07/agro-1080x675.jpg"
+						alt="Third slide"
+					/>
+
+					<Carousel.Caption>
+						<h3>Third slide label</h3>
+						<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
 		</div>
 	);
 };

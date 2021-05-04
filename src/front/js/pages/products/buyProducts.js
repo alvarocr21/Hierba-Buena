b427e5a15ef232d9fee0f00af6131ab32d38a350
alvarocr21 <alvarocr21@gmail.com>
2021-05-04 01:06:56 +0000

    vista de compra productos

--- src/front/js/pages/products/buyProducts.js
+++ src/front/js/pages/products/buyProducts.js
@@ -7,20 +7,69 @@ export const BuyProducts = () => {
 
 	return (
 		<div className="card-body ">
-			<img
-				src="https://s1.eestatic.com/2020/03/17/ciencia/nutricion/vegetales-transgenicos-tomate_475465019_148545151_1024x576.jpg"
-				className="card-img-top"
-				alt="..."
-			/>
+			<div className="row border border-primary">
+				<div className="col border border-secondary">
+					<div className="row border border-success">
+						<div className="col">
+							<img
+								src="https://s1.eestatic.com/2020/03/17/ciencia/nutricion/vegetales-transgenicos-tomate_475465019_148545151_1024x576.jpg"
+								className="card-img-top rounded-circle"
+								alt="..."
+							/>
+						</div>
+					</div>
+					<div className="row border border-success">
+						<div className="col">Tomate</div>
+						<div className="col">Precio</div>
+					</div>
+					<div className="row border border-success">
+						<div className="col"> 2 Kg</div>
+						<div className="col">1300</div>
+					</div>
+				</div>
+				<div className="col border border-secondary">
+					<div className="row border border-success">
+						<div className="col">
+							<div className="col">
+								<img
+									src="https://www.elmundo.cr/wp-content/uploads/2020/02/2020-01-15-11.39.43-1024x768.jpg"
+									className="card-img-top rounded-circle"
+									alt="..."
+								/>
+							</div>
+						</div>
+					</div>
+					<div className="row border border-success">
+						<div className="col">
+							<p className="card-text mb-1">Yarmain Montes</p>
+							<p className="card-text">
+								<small className="text-muted">Guapiles, Limón</small>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="row border border-primary">
+				<div className="col border border-secondary">total </div>
+				<div className="col border border-secondary">
+					<button className="btn btn-primary">Detalle</button>
+				</div>
+			</div>
 
-			<div className="card-body">
-				<h5 className="card-title">Card title</h5>
-				<p className="card-text">
-					Some quick example text to build on the card title and make up the bulk of the cards content.
-				</p>
-				<a href="#" className="btn btn-success float-right">
-					Go somewhere
-				</a>
+			<div className="row border border-primary">
+				<div className="col border border-secondary">
+					<div className="row">
+						<div className="col border border-secondary">
+							<button className="btn btn-success">+</button>
+						</div>
+						<div className="col border border-secondary">
+							<button className="btn btn-danger">-</button>
+						</div>
+					</div>
+				</div>
+				<div className="col border border-secondary">
+					<button className="btn btn-success">Agregar</button>
+				</div>
 			</div>
 		</div>
 	);

dc5df7911a725673687c457dd8ea6b297b084465
josecalcru <josecalcru@gmail.com>
2021-05-14 01:02:52 +0000

    landing page update

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -19,7 +19,7 @@ export const Home = () => {
 			<div id="frame">
 				<img id="myImg" src={logo} />
 			</div>
-			<div className="text-center container-fluid ">
+			<div className="text-center container-fluid mt-4">
 				<div className="row mobileDiagramDiv">
 					<div className="col-7 col-md-3 mr-auto">
 						<img id="diagramMobile" src={diagramMobile} />
@@ -64,44 +64,6 @@ export const Home = () => {
 						</div>
 					</div>
 				</div>
-				<Carousel className="mx-3 my-3">
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://www.biotropic.com/images/news/2019/beitragsbilder/PPP-CostaRica_1200px.jpg"
-							alt="First slide"
-						/>
-						<Carousel.Caption>
-							<h3>First slide label</h3>
-							<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-						</Carousel.Caption>
-						<p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-					</Carousel.Item>
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://www.lateja.cr/resizer/0fNaS7QleEmBP1NS4ltTcBntQbw=/600x0/center/middle/filters:quality(100)/arc-anglerfish-arc2-prod-gruponacion.s3.amazonaws.com/public/WOZTXRYOHZBPZPYQOKPAU7ECHM.JPG"
-							alt="Second slide"
-						/>
-
-						<Carousel.Caption>
-							<h3>Second slide label</h3>
-							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-					<Carousel.Item>
-						<img
-							className="d-block w-100"
-							src="https://revistasumma.com/wp-content/uploads/2019/07/agro-1080x675.jpg"
-							alt="Third slide"
-						/>
-
-						<Carousel.Caption>
-							<h3>Third slide label</h3>
-							<p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-						</Carousel.Caption>
-					</Carousel.Item>
-				</Carousel>
 			</div>
 		</div>
 	);

51a3a122c7653c585ef402ff1a85070116405e23
josecalcru <josecalcru@gmail.com>
2021-05-14 04:06:39 +0000

    landing page diagram update for mobiles

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -5,6 +5,10 @@ import { Link } from "react-router-dom";
 import landingimg1 from "../../img/landingPage/landing 1.jpg";
 import logo from "../../img/landingPage/Hierbabuena-verde.png";
 import diagramMobile from "../../img/landingPage/diagramMobile.png";
+import agricultor from "../../img/landingPage/agricultor.png";
+import receiving from "../../img/landingPage/receiving.png";
+import downArrow from "../../img/landingPage/downArrow.png";
+import hierbaTelefono from "../../img/landingPage/hierbaTelefono.png";
 import mainDiagram from "../../img/landingPage/mainDiagram.png";
 import veggieCar from "../../img/landingPage/veggieCar.png";
 import horizontalBackground from "../../img/landingPage/landingHorizontalBackground.jpg";
@@ -20,8 +24,8 @@ export const Home = () => {
 				<img id="myImg" src={logo} />
 			</div>
 			<div className="text-center container-fluid">
-				<div className="row mobileDiagramDiv">
-					<div className="col-7 mr-auto p-0">
+				<div className="col mobileDiagramDiv">
+					{/* <div className="col-7 mr-auto p-0">
 						<img id="diagramMobile" src={diagramMobile} />
 					</div>
 					<div className="col-5">
@@ -40,6 +44,48 @@ export const Home = () => {
 						<div className="diagramtext3">
 							<p>... y reciba sus productos frescos! </p>
 						</div>
+					</div> */}
+					<div className="row">
+						<div className="col-6 mr-auto">
+							<img className="m-auto" id="agricultor" src={agricultor} />
+						</div>
+						<div className="col-6 ml-auto diagramtext1">
+							<p className="m-auto">¡Apoye a sus agricultores locales!</p>
+						</div>
+					</div>
+					<div className="row mt-3">
+						<div className="col-6 mr-auto">
+							<img id="downArrow" src={downArrow} />
+						</div>
+					</div>
+					<div className="row mt-3">
+						<div className="col-6 mr-auto">
+							<img id="hierbaTelefono" src={hierbaTelefono} />
+						</div>
+						<div className="col-6 ml-auto diagramtext2">
+							<p>
+								{" "}
+								Ingrese a nuestra{" "}
+								<Link to="/Products" className="link">
+									{" "}
+									<button className="btn btn-success">tienda </button>
+								</Link>
+							</p>
+						</div>
+					</div>
+
+					<div className="row mt-3">
+						<div className="col-6 mr-auto">
+							<img id="downArrow" src={downArrow} />
+						</div>
+					</div>
+					<div className="row mt-3">
+						<div className="col-6 mr-auto">
+							<img id="receiving" src={receiving} />
+						</div>
+						<div className="col-6 ml-auto diagramtext3">
+							<p className="m-auto">... y reciba sus productos frescos!</p>
+						</div>
 					</div>
 				</div>
 				<div className="col diagramDiv">
@@ -50,14 +96,8 @@ export const Home = () => {
 						<div className="col-md-4 diagramtext3">
 							<p id="diagramtext3">... y reciba sus productos frescos! </p>
 						</div>
-						<div className="col-md-4 diagramtext2">
-							<p>
-								Ingrese a nuestra{" "}
-								<Link to="/Products" className="link">
-									{" "}
-									<button className="btn btn-success">tienda </button>
-								</Link>
-							</p>
+						<div className="col-md-4 diagramtext3">
+							<p>Ingrese a nuestra </p>
 						</div>
 						<div className="col-md-4 diagramtext1">
 							<p id="diagramtext1">¡Apoye a sus agricultores locales!</p>
